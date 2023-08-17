f825c402a68718ae8a7a015249fd8e102dce6155
Sony <Sony@DESKTOP-AG0407A>
2023-08-17 10:39:49 +0530

    first commit

---
+++ src/test/java/maven_project/CreateOrder.java
@@ -0,0 +1,12 @@
+package maven_project;
+
+import org.testng.annotations.Test;
+
+public class CreateOrder {
+	
+	@Test(groups="smoke")
+	public void createOrder() {
+		System.out.println("order has been created");
+	}
+
+}
---
+++ src/test/java/maven_project/ModifyOrder.java
@@ -0,0 +1,12 @@
+package maven_project;
+
+import org.testng.annotations.Test;
+
+public class ModifyOrder {
+	
+	@Test
+	public void modifyOrder() {
+		System.out.println("order has been modified");
+	}
+
+}
