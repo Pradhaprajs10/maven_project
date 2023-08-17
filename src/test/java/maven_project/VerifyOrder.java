87fdf2669b0c903270ba268d73cca0e93a39de84
pradhaprajs10 <pradhaprajs10@gmail.com>
2023-08-17 21:45:15 +0530

    commit verify order to branch 1

---
+++ src/test/java/maven_project/VerifyOrder.java
@@ -0,0 +1,12 @@
+package maven_project;
+
+import org.testng.annotations.Test;
+
+public class VerifyOrder {
+	
+	@Test
+	public void verifyOrder() {
+		System.out.println("order has been verified");
+	}
+
+}
